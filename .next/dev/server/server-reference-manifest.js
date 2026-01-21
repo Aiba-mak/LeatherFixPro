c720f091f8990e1ff11a733129a3ae988aab12df
Aiba Mak <aiba.mak94@gmail.com>
2026-01-20 23:47:20 -0800

    Fix mobile background and quote layout

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"BE460twV2iQpfEJM9QDzkXm0DioxLGebNsp2AeitMRs=\"\n}"
--- .next/server/chunks/ssr/_1d59926b._.js
+++
@@ -1,3 +0,0 @@
-module.exports=[71029,(a,b,c)=>{"use strict";c._=function(a){return a&&a.__esModule?a:{default:a}}},62243,a=>{a.n(a.i(49575))},43619,a=>{a.n(a.i(79962))},13718,a=>{a.n(a.i(85523))},18198,a=>{a.n(a.i(45518))}];
-
-//# sourceMappingURL=_1d59926b._.js.map
