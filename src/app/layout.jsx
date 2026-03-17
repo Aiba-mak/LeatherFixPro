f0b6c0308be95ea8e823c33e60ad40640712cd28
Aibek <67276092+Aiba-mak@users.noreply.github.com>
2026-03-16 23:40:04 -0700

    Integrate Google Tag Manager in layout component
    Added Google Tag Manager scripts for tracking.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,3 +1,4 @@
+import Script from "next/script"
 import "./globals.css";
 import { Header } from "../components/Header";
 
@@ -5,6 +6,19 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <Script
+  src="https://www.googletagmanager.com/gtag/js?id=AW-17952923120"
+  strategy="afterInteractive"
+/>
+
+<Script id="google-ads">
+{`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'AW-17952923120');
+`}
+</Script>
         <Header />
         {children}
       </body>
